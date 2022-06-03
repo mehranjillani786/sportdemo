88db59ae19eac78208b40b24876568f093ac9b5a
mehranjillani786 <mehran.jillani54@gmail.com>
2022-06-03 23:19:02 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -10,16 +10,14 @@ import { Hidden } from "@material-ui/core";
 function App() {
   return (
     <>
-    <h1>Hello</h1>
-    <h5>Author: Mehran</h5>
       {/* <Tabs /> */}
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* <Hidden smUp> */}
           <Routes>
-            {/* <Route path='/' element={ <Navigate to="/group" replace={true} />}/> */}
-            <Route path="/" element={<Tabs />} />
-            <Route path="/group/:id" element={<UnderGroups />} />
-            <Route path="/sports/:id" element={<UnderGroupDetail />} />
+            <Route path='/' element={ <Navigate to="group" replace={true} />}/>
+            <Route path="group" element={<Tabs />} />
+            <Route path="group/:id" element={<UnderGroups />} />
+            <Route path="sports/:id" element={<UnderGroupDetail />} />
           </Routes>
         {/* </Hidden> */}
         {/* <Hidden xsDown>

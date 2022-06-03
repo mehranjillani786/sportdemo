851cbda207b2ecb12c938837defb12127a74b17d
mehranjillani786 <mehran.jillani54@gmail.com>
2022-06-03 22:49:23 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* <Hidden smUp> */}
           <Routes>
-            <Route path='/' element={ <h1>author: mehran</h1>}/>
-            <Route path="/group" element={<Tabs />} />
+            {/* <Route path='/' element={ <h1>author: mehran</h1>}/> */}
+            <Route path="/" element={<Tabs />} />
             <Route path="/group/:id" element={<UnderGroups />} />
             <Route path="/sports/:id" element={<UnderGroupDetail />} />
           </Routes>
